50a4669c0cd80b483cbcdfdd93e538ec9e2b2547
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-12-26 15:02:17 +0700

    update composer

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -18,8 +18,8 @@ namespace nguyenanhung\Classes\Helper;
  */
 interface ProjectInterface
 {
-    const VERSION       = '1.0.2.12';
-    const LAST_MODIFIED = '2018-12-25';
+    const VERSION       = '1.0.2.13';
+    const LAST_MODIFIED = '2018-12-26';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = '30s of PHP Code';
